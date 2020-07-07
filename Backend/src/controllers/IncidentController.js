ad99e117df2fbfaacde5ce188e3660ecb752b20e
RockpunkZikaGG <william.rocha.silva.wrs@gmail.com>
2020-07-07 13:53:56 -0300

    Frontend Web

--- Backend/src/controllers/IncidentController.js
+++ Backend/src/controllers/IncidentController.js
@@ -42,8 +42,12 @@ module.exports = {
 
         const incident = await connection('incidents').where('id', id).select("ong_id").first();
 
-        if(incident != ong_id ){
-            return res.status(401).json({ error: 'Operation not permitted.'});
+        try{
+            if(incident.ong_id != ong_id ){
+                return res.status(401).json({ error: 'Operation not permitted.'});
+            }
+        }catch{
+            return res.status(400).json({ error: 'Erro ao realizar operação.'});
         }
         await connection('incidents').where('id', id).delete();
 
